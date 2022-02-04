506cc3d85e3dd49125db60c6c140549c3e3fc051
LauraFernandez18 <10000068.joan23@fje.edu>
2022-02-04 17:24:50 +0100

    validacion js

--- public/js/validacion.js
+++ public/js/validacion.js
@@ -1,1 +1,20 @@
-alert("hola");
+function validar_user() {
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+    mensaje = document.getElementById('mensaje')
+
+    if (email == '' && password == '') {
+        mensaje.innerHTML = 'Introduce el email y la contraseña'
+        return false
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        mensaje.innerHTML = 'Introduce un email correcto'
+        return false
+    } else if (email == '') {
+        mensaje.innerHTML = 'Introduce el email'
+    } else if (password == '') {
+        mensaje.innerHTML = 'Introduce la contraseña'
+        return false
+    } else {
+        return true
+    }
+}
--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device=width, initial-scale=1.0">
-    <script type="text/javascript" src="../../js/validacion.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script type="text/javascript" src="js/validacion.js"></script>
     <title>Registro</title>
 </head>
 <body>
