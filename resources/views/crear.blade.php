0eca5b0497a1bca9211a297d276e61a0fca9442d
David <6694.joan23@fje.edu>
2022-02-07 19:40:06 +0100

    validacions JS

--- resources/views/crear.blade.php
+++ resources/views/crear.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device=width, initial-scale=1.0">
+    <script src="{!! asset('js/validacion.js') !!}"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>Crear</title>
 </head>
 <body>
@@ -16,40 +18,40 @@
         </ul>
     </div>
     @endif
-    <form action="{{url('crearPost')}}" method="POST" enctype="multipart/form-data">
+    <form action="{{url('crearPost')}}" method="POST" enctype="multipart/form-data" onsubmit="return validarCrear();">
         @csrf
         <p>Nombre</p>
-        <input type="text" name="nombre" placeholder="Introduce el nombre..." value="{{old('nombre')}}">
+        <input type="text" id="nombre" name="nombre" placeholder="Introduce el nombre..." value="{{old('nombre')}}">
         @error('nombre')
         <br>
         {{$message}}
         @enderror
         <p>Precio</p>
-        <input type="text" name="precio" placeholder="Introduce el precio...">
+        <input type="text" id="precio" name="precio" placeholder="Introduce el precio...">
         @error('precio')
         <br>
         {{$message}}
         @enderror
         <p>Nacionalidad</p>
-        <input type="text" name="nacionalidad" placeholder="Introduce la nacionalidad...">
+        <input type="text" id="nacionalidad" name="nacionalidad" placeholder="Introduce la nacionalidad...">
         @error('nacionalidad')
         <br>
         {{$message}}
         @enderror
         <p>Tipo</p>
-        <input type="text" name="tipo" placeholder="Introduce el primer tipo...">
+        <input type="text" id="tipo" name="tipo" placeholder="Introduce el primer tipo...">
         @error('nacionalidad')
         <br>
         {{$message}}
         @enderror
-        <p>Segundo tipo</p>
+        <p>Segundo tipo (Opcional)</p>
         <input type="text" name="tipo2" placeholder="Introduce el segundo tipo...">
         @error('nacionalidad')
         <br>
         {{$message}}
         @enderror
         <p>Foto</p>
-        <input type="file" name="foto">
+        <input type="file" id="foto" name="foto">
         @error('foto')
         <br>
         {{$message}}
--- resources/views/modificar.blade.php
+++ resources/views/modificar.blade.php
@@ -4,24 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device=width, initial-scale=1.0">
+    <script src="{!! asset('js/validacion.js') !!}"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>Modificar</title>
 </head>
 <body>
-    <form action="{{url('modificarPut')}}" method="POST" enctype="multipart/form-data">
+    <form action="{{url('modificarPut')}}" method="POST" enctype="multipart/form-data" onsubmit="return validarModificar();">
         @csrf
         {{method_field('PUT')}}
         <p>Nombre</p>
-        <input type="text" name="nombre" value="{{$restaurante->nombre}}">
+        <input type="text" id="nombre" name="nombre" value="{{$restaurante->nombre}}">
         <p>Precio</p>
-        <input type="text" name="precio" value="{{$restaurante->precio}}">
+        <input type="text" id="precio" name="precio" value="{{$restaurante->precio}}">
         <p>Nacionalidad</p>
-        <input type="text" name="nacionalidad" value="{{$restaurante->nacionalidad}}">
+        <input type="text" id="nacionalidad" name="nacionalidad" value="{{$restaurante->nacionalidad}}">
         <p>Tipo</p>
-        <input type="text" name="tipo" value="{{$restaurante->tipo}}">
+        <input type="text" id="tipo" name="tipo" value="{{$restaurante->tipo}}">
         <p>Segundo tipo</p>
-        <input type="text" name="tipo2" value="{{$restaurante->tipo2}}">
+        <input type="text" id="tipo2" name="tipo2" value="{{$restaurante->tipo2}}">
         <p>Foto</p>
-        <input type="file" name="foto" value="{{$restaurante->foto}}">
+        <input type="file" id="foto" name="foto" value="{{$restaurante->foto}}">
         <div>
             <input type="hidden" name="id" value="{{$restaurante->id}}">
             <input type="submit" name="enviar">
