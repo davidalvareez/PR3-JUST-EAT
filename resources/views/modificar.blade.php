637817a7330aefcb044152b15c8863b93d7e38b9
David <6694.joan23@fje.edu>
2022-02-08 20:53:39 +0100

    bugs estilos ok

--- resources/views/modificar.blade.php
+++ resources/views/modificar.blade.php
@@ -11,7 +11,7 @@
     <script src="{!! asset('js/validacion.js') !!}"></script>
     <title>Just Eaten</title>
 </head>
-<body>
+<body class="login">
     @if($errors->any())
     <div>
         <ul>
@@ -21,50 +21,43 @@
         </ul>
     </div>
     @endif
-    <form action="{{url('modificarPut')}}" method="POST" enctype="multipart/form-data" onsubmit="return validarModificar();">
-        @csrf
-        {{method_field('PUT')}}
-        <p>Nombre</p>
-        <input type="text" name="nombre" value="{{$restaurante->nombre}}">
-        @error('nombre')
-        <br>
-        {{$message}}
-        @enderror
-        <p>Precio</p>
-        <input type="text" name="precio" value="{{$restaurante->precio}}">
-        @error('precio')
-        <br>
-        {{$message}}
-        @enderror
-        <p>Descripción</p>
-        <textarea name="descripcion" rows="5" cols="30">{{$restaurante->descripcion}}</textarea>
-        @error('descripcion')
-        <br>
-        {{$message}}
-        @enderror
-        <p>Nacionalidad</p>
-        <input type="text" name="nacionalidad" value="{{$restaurante->nacionalidad}}">
-        @error('nacionalidad')
-        <br>
-        {{$message}}
-        @enderror
-        <p>Tipo</p>
-        <input type="text" name="tipo" value="{{$restaurante->tipo}}">
-        @error('tipo')
-        <br>
-        {{$message}}
-        @enderror
-        <p>Segundo tipo</p>
-        <input type="text" name="tipo2" value="{{$restaurante->tipo2}}">
-        @error('tipo2')
-        <br>
-        {{$message}}
-        @enderror
-        <p>Foto</p>
-        <input type="file" name="foto" value="{{$restaurante->foto}}">
-        <div>
-            <input type="hidden" name="id" value="{{$restaurante->id}}">
-            <input class="inputregistro"type="submit" value="Modificar restaurante">
+    <div class="row flex-cv">
+        <div class="cuadro_modificar_restaurante">
+            <form action="{{url('modificarPut')}}" method="POST" enctype="multipart/form-data" onsubmit="return validarModificar();">
+                @csrf
+                {{method_field('PUT')}}
+
+                <h1 class="h1_crear_restaurante">MODIFICAR RESTAURANTE</h1>
+                <input  class="input_crear_restaurante" type="text" name="nombre" value="{{$restaurante->nombre}}">
+                @error('nombre')
+                {{$message}}
+                @enderror
+                <input class="input_crear_restaurante" type="text" name="precio" value="{{$restaurante->precio}}">
+                @error('precio')
+                {{$message}}
+                @enderror
+                <textarea  class="input_crear_restaurante" style="resize:none" name="descripcion" rows="5" cols="30">{{$restaurante->descripcion}}</textarea>
+                @error('descripcion')
+                {{$message}}
+                @enderror
+                <input class="input_crear_restaurante" type="text" name="nacionalidad" value="{{$restaurante->nacionalidad}}">
+                @error('nacionalidad')
+                {{$message}}
+                @enderror
+                <input class="input_crear_restaurante" type="text" name="tipo" value="{{$restaurante->tipo}}">
+                @error('tipo')
+                {{$message}}
+                @enderror
+                <input class="input_crear_restaurante" type="text" name="tipo2" value="{{$restaurante->tipo2}}">
+                @error('tipo2')
+                {{$message}}
+                @enderror
+                <input class="input_crear_restaurante" type="file" name="foto" value="{{$restaurante->foto}}">
+                <div>
+                    <input type="hidden" name="id" value="{{$restaurante->id}}">
+                    <input class="inputregistro" type="submit" value="Modificar restaurante">
+                </div>
+            </div>
         </div>
     </div>
 </body>
